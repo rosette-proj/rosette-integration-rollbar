ec5f57a69ab642a050358a9f86bc91dfd4017ecf
Cameron Dutro <camertron@gmail.com>
2014-10-29 21:03:02 -0700

    Handle expected errors

--- lib/rosette/integrations/rollbar_integration.rb
+++ lib/rosette/integrations/rollbar_integration.rb
@@ -5,8 +5,9 @@ require 'rollbar'
 
 module Rosette
   module Integrations
+
     class RollbarIntegration < Integration
-      autoload :Configurator,   'rosette/integrations/rollbar_integration/configurator'
+      autoload :Configurator, 'rosette/integrations/rollbar_integration/configurator'
 
       def integrate(obj)
         if integrates_with?(obj)
@@ -24,12 +25,36 @@ module Rosette
         obj.ancestors.include?(Grape::API)
       end
 
+      def error(exception, **options)
+        configuration.rollbar_notifier.error(exception, options)
+      end
+
+      private
+
       def integrate_with_grape(obj)
         obj.endpoints.each do |endpoint|
           original_block = endpoint.block
+
           endpoint.block = lambda do |*args, &block|
+            env = args.first.env['api.endpoint']
+
             begin
-              original_block.call(endpoint)
+              response = catch(:error) do
+                # definitely counter-intuitive to pass endpoint to this method, but
+                # grape's insane metaprogramming spaghetti leaves us no choice
+                original_block.call(endpoint)
+              end
+
+              if response.fetch(:status, 200) >= 400
+                configuration.rollbar_notifier.error(
+                  response.fetch(:message, {}).fetch(:error, 'Unknown error'),
+                  env.params, env.headers
+                )
+
+                throw :error, response  # re-throw
+              end
+
+              response
             rescue => e
               env = args.first.env['api.endpoint']
               configuration.rollbar_notifier.error(e, env.params, env.headers)
@@ -38,11 +63,7 @@ module Rosette
           end
         end
       end
-
-      def error(exception, **options)
-        configuration.rollbar_notifier.error(exception, options)
-      end
-
     end
+
   end
 end
--- lib/rosette/integrations/rollbar_integration/configurator.rb
+++ lib/rosette/integrations/rollbar_integration/configurator.rb
@@ -3,14 +3,15 @@
 module Rosette
   module Integrations
     class RollbarIntegration < Integration
+
       class Configurator
         attr_reader :rollbar_notifier
 
         def set_rollbar_notifier(notifier)
           @rollbar_notifier = notifier
         end
-
       end
+
     end
   end
 end
